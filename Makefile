593193686bbf67b2fe43d67ab3c3e73602ccac49
Johan Peeters <yo@johanpeeters.com>
2018-12-07 17:19:03 +0100

    trying to figure out how config will be passed to the client

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 all: build
 
+.env:
+	for
+
 build: src/* public/*
 	npm run build
+	for var in `env`; do echo $var >> build/static/js/.env
--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import './App.css'
+require('dotenv').config()
 
 class App extends Component {
   render() {
+    let msg = JSON.stringify(process.env)
     return (
       <div className="App">
         <header className="App-header">
@@ -10,8 +12,11 @@ class App extends Component {
             Ride Sharing
           </p>
         </header>
+        <p>
+          All environment variables: {msg}
+        </p>
       </div>
-    );
+    )
   }
 }
 
