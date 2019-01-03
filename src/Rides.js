fefb5aa7686b5f4dc1ec6ebd6feec4cb6d164605
Johan Peeters <yo@johanpeeters.com>
2019-01-03 18:20:24 +0100

    sort rides from earliest to latest

--- src/Rides.js
+++ src/Rides.js
@@ -2,21 +2,30 @@ import React from 'react'
 import RideForm from './RideForm'
 
 const Rides = (props) => {
-  return(
-    <div>
-      {props.list && props.list.map(ride =>
-        <RideForm data={ride}
+  if (props.list) {
+    const rides = props.list
+    rides.sort((ride1, ride2) => {
+      if (ride1.when < ride2.when) return -1
+      if (ride1.when > ride2.when) return 1
+      return 0
+    })
+    return(
+      <div>
+        {rides.map(ride =>
+          <RideForm data={ride}
               disabled={true}
               key={ride.id}
               done={props.update}/>
-      )}
-      {!props.list &&
-        <p>
-          Waiting for rides to load
-        </p>
-      }
-    </div>
-  )
+        )}
+      </div>
+    )
+  } else {
+    return (
+      <p>
+        Waiting for rides to load
+      </p>
+    )
+  }
 }
 
 export {Rides}
